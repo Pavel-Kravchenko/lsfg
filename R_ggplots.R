197b060d1f67b261d80efb482af5d0f56e9b8a40
Pavel <pavel-kravchenk0@yandex.ru>
2018-08-14 22:45:19 +0300

    Changed R script

--- R_ggplots.R
+++ R_ggplots.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(dplyr)
 
 table_clones_103 <- read_delim("~/Desktop/TCR/data/103_clones.txt", 
-                          "\t", escape_double = FALSE, trim_ws = TRUE)
+                               "\t", escape_double = FALSE, trim_ws = TRUE)
 
 
 table_clones_103 %>%
@@ -52,46 +52,83 @@ out_165 <- data.frame(aaSeqCDR3=table_clones_165$aaSeqCDR3, bestVGene=table_clon
 out_204 <- data.frame(aaSeqCDR3=table_clones_204$aaSeqCDR3, bestVGene=table_clones_204$bestVGene)
 out_205 <- data.frame(aaSeqCDR3=table_clones_205$aaSeqCDR3, bestVGene=table_clones_205$bestVGene)
 
+
+
 print(length(out_113[[1]]) + length(out_137[[1]]) + length(out_143[[1]]) + length(out_204[[1]]))
-merged_SF <- merge(out_113, out_137, by=c("aaSeqCDR3", "bestVGene"))
-merged_SF <- merge(merged_SF, out_143, by=c("aaSeqCDR3", "bestVGene"))
-merged_SF <- merge(merged_SF, out_204, by=c("aaSeqCDR3", "bestVGene"))
-merged_SF
-print(length(merged_SF[[1]]))
 
-print(length(out_103[[1]]) + length(out_135[[1]]) + length(out_165[[1]]) + length(out_205[[1]]))
-merged_PB <- merge(out_103, out_135, by=c("aaSeqCDR3", "bestVGene"))
-merged_PB <- merge(merged_PB, out_165, by=c("aaSeqCDR3", "bestVGene"))
-merged_PB <- merge(merged_PB, out_205, by=c("aaSeqCDR3", "bestVGene"))
-merged_PB
-print(length(merged_PB[[1]]))
+merged_1 <- merge(out_103, out_113, by=c("aaSeqCDR3", "bestVGene"))
+n_1 <- length(out_103[[1]]) + length(out_113[[1]])
+print(n_1)
+x_1 <- length(merged_1[[1]])
+print(x_1)
+bt_1 = binom.test(x = x_1, n = n_1, p = 0.5, alternative = 'less')
+bt_1$p.value
 
-merged <- merge(merged_PB, merged_SF, by=c("aaSeqCDR3", "bestVGene"))
-print(length(merged[[1]]))
+merged_2 <- merge(out_135, out_137, by=c("aaSeqCDR3", "bestVGene"))
+n_2 <- length(out_135[[1]]) + length(out_137[[1]])
+print(n_2)
+x_2 <- length(merged_2[[1]])
+print(x_2)
+bt_2 = binom.test(x = x_2, n = n_2, p = 0.5, alternative = 'less')
+bt_2$p.value
+
+merged_3 <- merge(out_143, out_165, by=c("aaSeqCDR3", "bestVGene"))
+n_3 <- length(out_143[[1]]) + length(out_165[[1]])
+print(n_3)
+x_3 <- length(merged_3[[1]])
+print(x_3)
+bt_3 = binom.test(x = x_3, n = n_3, p = 0.5, alternative = 'less')
+bt_3$p.value
+
+merged_4 <- merge(out_204, out_205, by=c("aaSeqCDR3", "bestVGene"))
+n_4 <- length(out_204[[1]]) + length(out_205[[1]])
+print(n_4)
+x_4 <- length(merged_4[[1]])
+print(x_4)
+bt_4 = binom.test(x = x_4, n = n_4, p = 0.5, alternative = 'less')
+bt_4$p.value
+
+
 
 # ntCDR3+Vgene
-out_103_2 <- data.frame(nSeqCDR3=table_clones_103$nSeqCDR3, bestVGene=table_clones_103$bestVGene)
-out_113_2 <- data.frame(nSeqCDR3=table_clones_113$nSeqCDR3, bestVGene=table_clones_113$bestVGene)
-out_135_2 <- data.frame(nSeqCDR3=table_clones_135$nSeqCDR3, bestVGene=table_clones_135$bestVGene)
-out_137_2 <- data.frame(nSeqCDR3=table_clones_137$nSeqCDR3, bestVGene=table_clones_137$bestVGene)
-out_143_2 <- data.frame(nSeqCDR3=table_clones_143$nSeqCDR3, bestVGene=table_clones_143$bestVGene)
-out_165_2 <- data.frame(nSeqCDR3=table_clones_165$nSeqCDR3, bestVGene=table_clones_165$bestVGene)
-out_204_2 <- data.frame(nSeqCDR3=table_clones_204$nSeqCDR3, bestVGene=table_clones_204$bestVGene)
-out_205_2 <- data.frame(nSeqCDR3=table_clones_205$nSeqCDR3, bestVGene=table_clones_205$bestVGene)
+out_103 <- data.frame(nSeqCDR3=table_clones_103$nSeqCDR3, bestVGene=table_clones_103$bestVGene)
+out_113 <- data.frame(nSeqCDR3=table_clones_113$nSeqCDR3, bestVGene=table_clones_113$bestVGene)
+out_135 <- data.frame(nSeqCDR3=table_clones_135$nSeqCDR3, bestVGene=table_clones_135$bestVGene)
+out_137 <- data.frame(nSeqCDR3=table_clones_137$nSeqCDR3, bestVGene=table_clones_137$bestVGene)
+out_143 <- data.frame(nSeqCDR3=table_clones_143$nSeqCDR3, bestVGene=table_clones_143$bestVGene)
+out_165 <- data.frame(nSeqCDR3=table_clones_165$nSeqCDR3, bestVGene=table_clones_165$bestVGene)
+out_204 <- data.frame(nSeqCDR3=table_clones_204$nSeqCDR3, bestVGene=table_clones_204$bestVGene)
+out_205 <- data.frame(nSeqCDR3=table_clones_205$nSeqCDR3, bestVGene=table_clones_205$bestVGene)
 
-print(length(out_113_2[[1]]) + length(out_137_2[[1]]) + length(out_143_2[[1]]) + length(out_204_2[[1]]))
-merged_SF_2 <- merge(out_113_2, out_137_2, by=c("nSeqCDR3", "bestVGene"))
-merged_SF_2 <- merge(merged_SF_2, out_143_2, by=c("nSeqCDR3", "bestVGene"))
-merged_SF_2 <- merge(merged_SF_2, out_204_2, by=c("nSeqCDR3", "bestVGene"))
-merged_SF_2
-print(length(merged_SF_2[[1]]))
+merged_1 <- merge(out_103, out_113, by=c("nSeqCDR3", "bestVGene"))
+n_1 <- length(out_103[[1]]) + length(out_113[[1]])
+#print(n_1)
+x_1 <- length(merged_1[[1]])
+#print(x_1)
+bt_1 = binom.test(x = x_1, n = n_1, p = 0.5, alternative = 'less')
+bt_1$p.value
 
-print(length(out_103_2[[1]]) + length(out_135_2[[1]]) + length(out_165_2[[1]]) + length(out_205_2[[1]]))
-merged_PB_2 <- merge(out_103_2, out_135_2, by=c("nSeqCDR3", "bestVGene"))
-merged_PB_2 <- merge(merged_PB_2, out_165_2, by=c("nSeqCDR3", "bestVGene"))
-merged_PB_2 <- merge(merged_PB_2, out_205_2, by=c("nSeqCDR3", "bestVGene"))
-merged_PB_2
-print(length(merged_PB_2[[1]]))
+merged_2 <- merge(out_135, out_137, by=c("nSeqCDR3", "bestVGene"))
+n_2 <- length(out_135[[1]]) + length(out_137[[1]])
+#print(n_2)
+x_2 <- length(merged_2[[1]])
+#print(x_2)
+bt_2 = binom.test(x = x_2, n = n_2, p = 0.5, alternative = 'less')
+bt_2$p.value
 
-merged_2 <- merge(merged_PB_2, merged_SF_2, by=c("nSeqCDR3", "bestVGene"))
-print(length(merged_2[[1]]))
+merged_3 <- merge(out_143, out_165, by=c("nSeqCDR3", "bestVGene"))
+n_3 <- length(out_143[[1]]) + length(out_165[[1]])
+#print(n_3)
+x_3 <- length(merged_3[[1]])
+#print(x_3)
+bt_3 = binom.test(x = x_3, n = n_3, p = 0.5, alternative = 'less')
+bt_3$p.value
+
+merged_4 <- merge(out_204, out_205, by=c("nSeqCDR3", "bestVGene"))
+n_4 <- length(out_204[[1]]) + length(out_205[[1]])
+#print(n_4)
+x_4 <- length(merged_4[[1]])
+#print(x_4)
+bt_4 = binom.test(x = x_4, n = n_4, p = 0.5, alternative = 'less')
+bt_4$p.value
+
